40407ac3843e23fe8da9807e1be3b615b4667e4f
Nicky Marcelli <nicky.m@aluno.ifsp.edu.br>
2023-09-20 12:51:02 -0300

    o

--- Aula1/Form1.Designer.cs
+++ Aula1/Form1.Designer.cs
@@ -45,13 +45,24 @@
             this.mtbTelefone = new System.Windows.Forms.TextBox();
             this.mtbHE = new System.Windows.Forms.TextBox();
             this.mtbHS = new System.Windows.Forms.TextBox();
+            this.EDITAR = new System.Windows.Forms.Button();
+            this.tabela = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CPF = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.telefone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sala = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.horarioentrada = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.horariosaida = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.senha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.data = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // btnMenssage
             // 
             this.btnMenssage.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.btnMenssage.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMenssage.Location = new System.Drawing.Point(59, 405);
+            this.btnMenssage.Location = new System.Drawing.Point(192, 442);
             this.btnMenssage.Name = "btnMenssage";
             this.btnMenssage.Size = new System.Drawing.Size(109, 26);
             this.btnMenssage.TabIndex = 9;
@@ -77,14 +88,14 @@
             this.txbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbNome.Location = new System.Drawing.Point(59, 92);
             this.txbNome.Name = "txbNome";
-            this.txbNome.Size = new System.Drawing.Size(242, 20);
+            this.txbNome.Size = new System.Drawing.Size(530, 20);
             this.txbNome.TabIndex = 1;
             this.txbNome.TextChanged += new System.EventHandler(this.txbNome_TextChanged);
             // 
             // txbSenha
             // 
             this.txbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbSenha.Location = new System.Drawing.Point(59, 369);
+            this.txbSenha.Location = new System.Drawing.Point(59, 266);
             this.txbSenha.Name = "txbSenha";
             this.txbSenha.PasswordChar = '*';
             this.txbSenha.Size = new System.Drawing.Size(242, 20);
@@ -96,7 +107,7 @@
             this.lblSenha.AutoSize = true;
             this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSenha.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblSenha.Location = new System.Drawing.Point(57, 347);
+            this.lblSenha.Location = new System.Drawing.Point(57, 244);
             this.lblSenha.Name = "lblSenha";
             this.lblSenha.Size = new System.Drawing.Size(44, 13);
             this.lblSenha.TabIndex = 3;
@@ -158,7 +169,7 @@
             this.lblHE.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.lblHE.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHE.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblHE.Location = new System.Drawing.Point(58, 242);
+            this.lblHE.Location = new System.Drawing.Point(331, 188);
             this.lblHE.Name = "lblHE";
             this.lblHE.Size = new System.Drawing.Size(130, 13);
             this.lblHE.TabIndex = 36;
@@ -170,7 +181,7 @@
             this.lblHS.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.lblHS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHS.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblHS.Location = new System.Drawing.Point(58, 292);
+            this.lblHS.Location = new System.Drawing.Point(466, 189);
             this.lblHS.Name = "lblHS";
             this.lblHS.Size = new System.Drawing.Size(110, 13);
             this.lblHS.TabIndex = 38;
@@ -199,13 +210,13 @@
             this.mtbTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mtbTelefone.Location = new System.Drawing.Point(201, 153);
             this.mtbTelefone.Name = "mtbTelefone";
-            this.mtbTelefone.Size = new System.Drawing.Size(100, 20);
+            this.mtbTelefone.Size = new System.Drawing.Size(192, 20);
             this.mtbTelefone.TabIndex = 3;
             // 
             // mtbHE
             // 
             this.mtbHE.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mtbHE.Location = new System.Drawing.Point(61, 262);
+            this.mtbHE.Location = new System.Drawing.Point(334, 208);
             this.mtbHE.Name = "mtbHE";
             this.mtbHE.Size = new System.Drawing.Size(35, 20);
             this.mtbHE.TabIndex = 6;
@@ -213,10 +224,80 @@
             // mtbHS
             // 
             this.mtbHS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mtbHS.Location = new System.Drawing.Point(61, 314);
+            this.mtbHS.Location = new System.Drawing.Point(469, 211);
             this.mtbHS.Name = "mtbHS";
             this.mtbHS.Size = new System.Drawing.Size(35, 20);
             this.mtbHS.TabIndex = 7;
+            // 
+            // EDITAR
+            // 
+            this.EDITAR.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.EDITAR.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EDITAR.Location = new System.Drawing.Point(59, 442);
+            this.EDITAR.Name = "EDITAR";
+            this.EDITAR.Size = new System.Drawing.Size(109, 26);
+            this.EDITAR.TabIndex = 41;
+            this.EDITAR.Text = "EDITAR";
+            this.EDITAR.UseVisualStyleBackColor = false;
+            this.EDITAR.Click += new System.EventHandler(this.EDITAR_Click);
+            // 
+            // tabela
+            // 
+            this.tabela.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.nome,
+            this.CPF,
+            this.telefone,
+            this.sala,
+            this.horarioentrada,
+            this.horariosaida,
+            this.senha,
+            this.data});
+            this.tabela.HideSelection = false;
+            this.tabela.Location = new System.Drawing.Point(59, 330);
+            this.tabela.Name = "tabela";
+            this.tabela.Size = new System.Drawing.Size(530, 94);
+            this.tabela.TabIndex = 42;
+            this.tabela.UseCompatibleStateImageBehavior = false;
+            this.tabela.View = System.Windows.Forms.View.Details;
+            this.tabela.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.tabela.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tabela_MouseDoubleClick);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // nome
+            // 
+            this.nome.Text = "nome";
+            // 
+            // CPF
+            // 
+            this.CPF.Text = "CPF";
+            // 
+            // telefone
+            // 
+            this.telefone.Text = "telefone";
+            // 
+            // sala
+            // 
+            this.sala.Text = "sala";
+            // 
+            // horarioentrada
+            // 
+            this.horarioentrada.Text = "horarioentrada";
+            // 
+            // horariosaida
+            // 
+            this.horariosaida.Text = "horariosaida";
+            // 
+            // senha
+            // 
+            this.senha.Text = "senha";
+            // 
+            // data
+            // 
+            this.data.Text = "data";
             // 
             // Form1
             // 
@@ -224,6 +305,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.tabela);
+            this.Controls.Add(this.EDITAR);
             this.Controls.Add(this.mtbHS);
             this.Controls.Add(this.mtbHE);
             this.Controls.Add(this.mtbTelefone);
@@ -271,6 +354,17 @@
         private System.Windows.Forms.TextBox mtbTelefone;
         private System.Windows.Forms.TextBox mtbHE;
         private System.Windows.Forms.TextBox mtbHS;
+        private System.Windows.Forms.Button EDITAR;
+        private System.Windows.Forms.ListView tabela;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader nome;
+        private System.Windows.Forms.ColumnHeader CPF;
+        private System.Windows.Forms.ColumnHeader telefone;
+        private System.Windows.Forms.ColumnHeader sala;
+        private System.Windows.Forms.ColumnHeader horarioentrada;
+        private System.Windows.Forms.ColumnHeader horariosaida;
+        private System.Windows.Forms.ColumnHeader senha;
+        private System.Windows.Forms.ColumnHeader data;
     }
 }
 
