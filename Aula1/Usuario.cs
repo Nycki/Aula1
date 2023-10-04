b40cbf3e297fe9fc6020275cf716ab3898002d47
Nicky Marcelli <nicky.m@aluno.ifsp.edu.br>
2023-10-04 12:46:48 -0300

    0

--- Aula1/Usuario.cs
+++ Aula1/Usuario.cs
@@ -3,23 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Aula1
 {
     public class Usuario
     {
 
-        private int Id;
-        private string nome;
-        private int cpf;
-        private string telefone;
-        private string sala;
-        private DateTime data;
-        private string horarioentrada;
-        private string horariosaida;
-        private string senha;
+        private int _id;
+        private string _nome;
+        private string _cpf;
+        private string _telefone;
+        private string _sala;
+        private DateTime _data;
+        private string _horarioentrada;
+        private string _horariosaida;
+        private string _senha;
         
-                
+        public Usuario(string nome,
+                string cpf, 
+                string telefone, 
+                string sala, 
+                DateTime data, 
+                string horarioentrada, 
+                string horariosaida, 
+                string senha)
+        {
+            _nome = nome;
+            _cpf = cpf;
+            _telefone = telefone;
+            _sala = sala;
+            _horarioentrada = horarioentrada;
+            _horariosaida = horariosaida;
+            _senha = senha;
+        }
+        public int Id
+        {
+            set { _id = value; }
+            get { return _id; }
+
+        }
+        public string Nome
+        {
+            set { _nome = value; }
+            get { return _nome; }
+
+        }
+        public string Cpf
+        {
+            set { _cpf = value; }
+            get { return _cpf; }
+
+        }
+        public string Telefone
+        {
+            set { _telefone = value; }
+            get { return _telefone; }
+
+        }
+        public string Sala
+        {
+            set { _sala = value; }
+            get { return _sala; }
+
+        }
+        public DateTime Data
+        {
+            set { _data = value; }
+            get { return _data; }
+
+        }
+        public string Horarioentrada
+        {
+            set { _horarioentrada = value; }
+            get { return _horarioentrada; }
+
+        }
+        public string Horariosaida
+        {
+            set { _horariosaida = value; }
+            get { return _horariosaida; }
+
+        }
+        public string Senha
+        {
+            set { _senha = value; }
+            get { return _senha; }
+
+        }
 
     }
 }
---
+++ Aula1/UsuarioDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    internal class UsuarioDAO
+    {
+
+        public void DeletUsuario(int Id)
+        {
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"DELETE FROM SITE WHERE Id = @id";
+            sqlCommand.Parameters.AddWithValue("@id", Id);
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao excluir usuário no banco.\n" + err.Message);
+            }
+            finally
+            {
+                connection.CloseConnection();
+            }
+        }
+
+    }
+}
