1a4e3c0b1280eab3a992e41d0145578e1f4e0798
Nicky Marcelli <nicky.m@aluno.ifsp.edu.br>
2023-10-04 11:24:12 -0300

    o

---
+++ Aula1/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public class Usuario
+    {
+
+        private int Id;
+        private string nome;
+        private int cpf;
+        private string telefone;
+        private string sala;
+        private DateTime data;
+        private string horarioentrada;
+        private string horariosaida;
+        private string senha;
+        
+                
+
+    }
+}
